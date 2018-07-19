5d26c43762c53fa855209861a00c67b76ebd5a24
ayan4m1 <andrew@bulletlogic.com>
2018-07-19 16:06:32 -0400

    add VS project, move modlist files, add LICENSE

---
+++ ConanModSelector/ConanModSelector/MainForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ConanModSelector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdConanPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.cmdConanPathBrowse = new System.Windows.Forms.Button();
+            this.lblConanBrowse = new System.Windows.Forms.Label();
+            this.txtConanPath = new System.Windows.Forms.TextBox();
+            this.lblMods = new System.Windows.Forms.Label();
+            this.txtMods = new System.Windows.Forms.TextBox();
+            this.cmdModsBrowse = new System.Windows.Forms.Button();
+            this.fbdModPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblPreset = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmdWriteModlist = new System.Windows.Forms.Button();
+            this.clmPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmFilename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmInstalled = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listMods = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // fbdConanPath
+            // 
+            this.fbdConanPath.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // cmdConanPathBrowse
+            // 
+            this.cmdConanPathBrowse.Location = new System.Drawing.Point(315, 32);
+            this.cmdConanPathBrowse.Name = "cmdConanPathBrowse";
+            this.cmdConanPathBrowse.Size = new System.Drawing.Size(38, 20);
+            this.cmdConanPathBrowse.TabIndex = 0;
+            this.cmdConanPathBrowse.Text = "...";
+            this.cmdConanPathBrowse.UseVisualStyleBackColor = true;
+            this.cmdConanPathBrowse.Click += new System.EventHandler(this.cmdConanPathBrowse_Click);
+            // 
+            // lblConanBrowse
+            // 
+            this.lblConanBrowse.AutoSize = true;
+            this.lblConanBrowse.Location = new System.Drawing.Point(12, 35);
+            this.lblConanBrowse.Name = "lblConanBrowse";
+            this.lblConanBrowse.Size = new System.Drawing.Size(71, 13);
+            this.lblConanBrowse.TabIndex = 1;
+            this.lblConanBrowse.Text = "Conan Install:";
+            // 
+            // txtConanPath
+            // 
+            this.txtConanPath.Location = new System.Drawing.Point(89, 32);
+            this.txtConanPath.Name = "txtConanPath";
+            this.txtConanPath.Size = new System.Drawing.Size(220, 20);
+            this.txtConanPath.TabIndex = 2;
+            this.txtConanPath.Text = "E:\\steamapps\\common\\Conan Exiles";
+            this.txtConanPath.TextChanged += new System.EventHandler(this.txtConanPath_TextChanged);
+            // 
+            // lblMods
+            // 
+            this.lblMods.AutoSize = true;
+            this.lblMods.Location = new System.Drawing.Point(47, 61);
+            this.lblMods.Name = "lblMods";
+            this.lblMods.Size = new System.Drawing.Size(36, 13);
+            this.lblMods.TabIndex = 3;
+            this.lblMods.Text = "Mods:";
+            // 
+            // txtMods
+            // 
+            this.txtMods.Location = new System.Drawing.Point(89, 58);
+            this.txtMods.Name = "txtMods";
+            this.txtMods.Size = new System.Drawing.Size(220, 20);
+            this.txtMods.TabIndex = 4;
+            this.txtMods.Text = "E:\\steamapps\\workshop\\content\\440900";
+            this.txtMods.TextChanged += new System.EventHandler(this.txtMods_TextChanged);
+            // 
+            // cmdModsBrowse
+            // 
+            this.cmdModsBrowse.Location = new System.Drawing.Point(315, 58);
+            this.cmdModsBrowse.Name = "cmdModsBrowse";
+            this.cmdModsBrowse.Size = new System.Drawing.Size(38, 20);
+            this.cmdModsBrowse.TabIndex = 5;
+            this.cmdModsBrowse.Text = "...";
+            this.cmdModsBrowse.UseVisualStyleBackColor = true;
+            this.cmdModsBrowse.Click += new System.EventHandler(this.cmdModsBrowse_Click);
+            // 
+            // fbdModPath
+            // 
+            this.fbdModPath.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // lblPreset
+            // 
+            this.lblPreset.AutoSize = true;
+            this.lblPreset.Location = new System.Drawing.Point(12, 9);
+            this.lblPreset.Name = "lblPreset";
+            this.lblPreset.Size = new System.Drawing.Size(64, 13);
+            this.lblPreset.TabIndex = 9;
+            this.lblPreset.Text = "Mod Preset:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Softcore",
+            "Hardcore"});
+            this.comboBox1.Location = new System.Drawing.Point(89, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cmdWriteModlist
+            // 
+            this.cmdWriteModlist.Location = new System.Drawing.Point(359, 58);
+            this.cmdWriteModlist.Name = "cmdWriteModlist";
+            this.cmdWriteModlist.Size = new System.Drawing.Size(75, 20);
+            this.cmdWriteModlist.TabIndex = 11;
+            this.cmdWriteModlist.Text = "Write";
+            this.cmdWriteModlist.UseVisualStyleBackColor = true;
+            this.cmdWriteModlist.Click += new System.EventHandler(this.cmdWriteModlist_Click);
+            // 
+            // clmPath
+            // 
+            this.clmPath.Text = "Path";
+            this.clmPath.Width = 101;
+            // 
+            // clmID
+            // 
+            this.clmID.Text = "ID";
+            this.clmID.Width = 118;
+            // 
+            // clmFilename
+            // 
+            this.clmFilename.Text = "Filename";
+            this.clmFilename.Width = 112;
+            // 
+            // clmInstalled
+            // 
+            this.clmInstalled.Text = "Installed";
+            // 
+            // listMods
+            // 
+            this.listMods.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmPath,
+            this.clmID,
+            this.clmFilename,
+            this.clmInstalled});
+            this.listMods.Location = new System.Drawing.Point(12, 84);
+            this.listMods.Name = "listMods";
+            this.listMods.Size = new System.Drawing.Size(413, 383);
+            this.listMods.TabIndex = 6;
+            this.listMods.UseCompatibleStateImageBehavior = false;
+            this.listMods.View = System.Windows.Forms.View.Details;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 479);
+            this.Controls.Add(this.cmdWriteModlist);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblPreset);
+            this.Controls.Add(this.listMods);
+            this.Controls.Add(this.cmdModsBrowse);
+            this.Controls.Add(this.txtMods);
+            this.Controls.Add(this.lblMods);
+            this.Controls.Add(this.txtConanPath);
+            this.Controls.Add(this.lblConanBrowse);
+            this.Controls.Add(this.cmdConanPathBrowse);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Conan Mod Selector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdConanPath;
+        private System.Windows.Forms.Button cmdConanPathBrowse;
+        private System.Windows.Forms.Label lblConanBrowse;
+        private System.Windows.Forms.TextBox txtConanPath;
+        private System.Windows.Forms.Label lblMods;
+        private System.Windows.Forms.TextBox txtMods;
+        private System.Windows.Forms.Button cmdModsBrowse;
+        private System.Windows.Forms.FolderBrowserDialog fbdModPath;
+        private System.Windows.Forms.Label lblPreset;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button cmdWriteModlist;
+        private System.Windows.Forms.ColumnHeader clmPath;
+        private System.Windows.Forms.ColumnHeader clmID;
+        private System.Windows.Forms.ColumnHeader clmFilename;
+        private System.Windows.Forms.ColumnHeader clmInstalled;
+        private System.Windows.Forms.ListView listMods;
+    }
+}
+
---
+++ ConanModSelector/ConanModSelector/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConanModSelector
+{
+    public partial class MainForm : Form
+    {
+        List<int> modIds;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdConanPathBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbdConanPath.ShowDialog() == DialogResult.OK)
+            {
+                txtConanPath.Text = fbdConanPath.SelectedPath;
+            }
+        }
+
+        private void cmdModsBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbdModPath.ShowDialog() == DialogResult.OK)
+            {
+                txtMods.Text = fbdModPath.SelectedPath;
+            }
+        }
+
+        private void cmdWriteModlist_Click(object sender, EventArgs e)
+        {
+            if (modIds == null)
+            {
+                MessageBox.Show("Select a preset!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (txtMods.Text.Length == 0)
+            {
+                MessageBox.Show("Enter a mod path!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var modListFile = Path.Combine(txtConanPath.Text, "ConanSandbox", "Mods", "modlist.txt");
+            var modListData = "";
+            foreach (var modId in modIds)
+            {
+                var modGlob = Path.Combine(txtMods.Text, modId.ToString());
+                var files = Directory.EnumerateFiles(modGlob, "*.pak");
+                if (files.Count() > 1)
+                {
+                    Console.WriteLine($"WARN: More than one .pak in {modGlob}");
+                }
+
+                var file = files.First();
+                modListData += $"*{file}\r\n";
+            }
+            File.WriteAllText(modListFile, modListData);
+            MessageBox.Show($"Wrote {modIds.Count} mods to {modListFile}", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void txtConanPath_TextChanged(object sender, EventArgs e)
+        {
+            cmdWriteModlist.Enabled = txtConanPath.Text.Length > 0;
+        }
+
+        private void txtMods_TextChanged(object sender, EventArgs e)
+        {
+            cmdWriteModlist.Enabled = txtMods.Text.Length > 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Softcore")
+            {
+                modIds = new List<int>(new int[]
+                {
+                    864199675,
+                    1108556675,
+                    1183199682,
+                    1211904989,
+                    913496068,
+                    1369743238,
+                    1159180273,
+                    880177231,
+                    925197087,
+                    1437712973,
+                    862889805
+                });
+            }
+            else if (comboBox1.Text == "Hardcore")
+            {
+                modIds = new List<int>(new int[] {
+                    880454836,
+                    864199675,
+                    1183199682,
+                    1211904989,
+                    1108556675,
+                    1389681165,
+                    897947497,
+                    1159180273,
+                    1369743238,
+                    913496068,
+                    1417350098,
+                    925197087,
+                    862889805,
+                    1390768358,
+                    1315595341
+                });
+            }
+        }
+    }
+}
